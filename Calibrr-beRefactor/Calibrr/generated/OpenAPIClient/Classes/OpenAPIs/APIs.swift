9005b300b0130e54ec0e2ffc3cc2f163a5ee191c
CalibrrSocial <nolanbias99@gmail.com>
2026-01-16 12:00:00 -0500

    prifle icons saving again, need to make profile edit page go back after save

--- Calibrr-beRefactor/Calibrr/generated/OpenAPIClient/Classes/OpenAPIs/APIs.swift
+++ Calibrr-beRefactor/Calibrr/generated/OpenAPIClient/Classes/OpenAPIs/APIs.swift
@@ -14,7 +14,7 @@ import Foundation
 public typealias OpenAPIClient = OpenAPIClientAPI
 
 open class OpenAPIClientAPI {
-    public static var basePath = "https://api.calibrr.com/api"
+    public static var basePath = "http://18.220.142.127/api"
     public static var customHeaders: [String: String] = [:]
     public static var credential: URLCredential?
     public static var requestBuilderFactory: RequestBuilderFactory = URLSessionRequestBuilderFactory()
